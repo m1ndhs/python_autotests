006ff8e00174a10aa25286e575c6f50a1cf40b18
m1ndhs <mndlower@gmail.com>
2023-05-31 15:29:43 +0200

    Changed tests

--- tests/test_pokemon.py
+++ tests/test_pokemon.py
@@ -4,7 +4,7 @@ import requests
 def test_get_trainer_code():
     response = requests.get(url="https://pokemonbattle.me:9104/trainers",
                             headers={'content-type': 'application/json',
-                                     'trainer_token':"849b128a59aea9d37655d8d39d180262"})
+                                     'trainer_token':"ТОКЕН"})
 
     assert response.status_code == 200, f"Код ответа {response.status_code}, вместо 200"
 
@@ -13,10 +13,10 @@ def test_get_trainer_id():
 
     response = requests.get(url="https://pokemonbattle.me:9104/trainers",
                             headers={'content-type': 'application/json',
-                                     'trainer_token':"849b128a59aea9d37655d8d39d180262"},
-                            params={"trainer_id":"4311"})
+                                     'trainer_token':"ТОКЕН"},
+                            params={"trainer_id":"ВАШ_ID"})
 
-    assert response.json()["id"] == "4311", f"trainer_id не совпадает, 4311 != {response.json()['id']}"
+    assert response.json()["id"] == "ВАШ_ID", f"trainer_id не совпадает, ВАШ_ID != {response.json()['id']}"
 
 
 
